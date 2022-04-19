35b183aae4bc205ccb6f3336539811cf5f404b4e
Chuan Wang <chuan.wang101@gmail.com>
2022-04-19 13:53:16 -0700

    creat questions, prompt questions

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generate = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {type: "input", name:"title", message: "Enter the project title:"},
+    {type: "input", name:"descrip", message: "Enter a description of the project:"},
+    {type: "input", name:"install", message: "Enter installation instructions:"},
+    {type: "input", name:"usage", message: "Enter usage information:"},
+    {type: "input", name:"contrib", message: "Enter contribution guidelines:"},
+    {type: "input", name:"test", message: "Enter test instructions:"},
+    {type: "list", name:"license", message: "Choose a license:", choices: ["Apache License 2.0", "MIT License", "GNU GPLv3"]},
+    {type: "input", name:"username", message: "Enter your GitHub username:"},
+    {type: "input", name:"email", message: "Enter your contact email:"},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((res) => {
+        console.log(JSON.stringify(res));
+    });
+}
 
 // Function call to initialize app
 init();
-
-generate("hi");
