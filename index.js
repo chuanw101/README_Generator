2a600b1bdfd79f2135343507d7c6068a9cd760a4
Chuan Wang <chuan.wang101@gmail.com>
2022-04-19 13:05:11 -0700

    include generateMarkdown.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generate = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -13,3 +14,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+generate("hi");
